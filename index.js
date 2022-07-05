705e3c680e9858125ee7ba10083869eb0479bae8
katiuska Martinez <katiuskm.m@gmail.com>
2022-07-04 21:17:12 -0400

    nueva img UI

--- index.js
+++
@@ -1,8 +0,0 @@
-import barraScroll from "./barra_scroll.js";
-import hamburgerMenu from "./menu_hamburger.js";
-
-document.addEventListener("DOMContentLoaded",e=>{
-    hamburgerMenu(".hamburger",".panel",".menu a");
-    barraScroll();
-    
-});
